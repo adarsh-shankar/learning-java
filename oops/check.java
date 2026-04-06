d6e20821a4257b845c6d761c0783d14ec0ba6e2f
adarsh-shankar <adarshtripathi11@gmail.com>
2026-04-06 15:15:26 +0530

    deleting

--- oops/check.java
+++
@@ -1,6 +0,0 @@
-class oops {
-    public static void main(String[] args) {
-        System.out.println("Looking to see that whether it can create the subfolder ");
-    }
-    
-}
--- oops/oops.java
+++
@@ -1,6 +0,0 @@
-class oops{
-    public static void main(String[] args) {
-        System.out.println("My first approach to learn the java till its end with detail and keep the process fastest as possible");
-        System.out.println("Hello adarsh how are you hello world ");
-    }
-}
