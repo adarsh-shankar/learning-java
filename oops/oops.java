e686643f84594f0e3b672fca4ed295bc48712e1c
adarsh-shankar <adarshtripathi11@gmail.com>
2026-04-06 14:32:03 +0530

    first commit

---
+++ oops/oops.java
@@ -0,0 +1,6 @@
+class oops{
+    public static void main(String[] args) {
+        System.out.println("My first approach to learn the java till its end with detail and keep the process fastest as possible");
+
+    }
+}
